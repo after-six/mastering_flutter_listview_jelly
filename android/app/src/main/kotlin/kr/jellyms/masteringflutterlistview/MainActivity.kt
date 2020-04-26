b803d1f6795054d226f26e68a6553bb5d50091c6
jelly <chm073@gmail.com>
2020-04-26 21:04:03 +0900

    Static Listview

---
+++ android/app/src/main/kotlin/kr/jellyms/masteringflutterlistview/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.jellyms.masteringflutterlistview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ListViews',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('ListViews')),
+        body: BodyLayout(),
+      ),
+    );
+  }
+}
+
+class BodyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView(context);
+  }
+}
+
+// replace this function with the code in the examples
+Widget _myListView(BuildContext context) {
+  return ListView(
+    children: ListTile.divideTiles(
+      context: context,
+      tiles: [
+        ListTile(title: Text('Sun')),
+        ListTile(title: Text('Moon')),
+        ListTile(title: Text('Star')),
+      ],
+    ).toList(),
+  );
+}
